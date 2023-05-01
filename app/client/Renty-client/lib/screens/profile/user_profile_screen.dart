d8297943c645c192fb338bdc4f2c7af92771738d
jessi <vadim02101@gmail.com>
2023-05-01 17:01:57 +0300

    created a user profile screen

--- app/client/Renty-client/lib/screens/profile/user_profile_screen.dart
+++ app/client/Renty-client/lib/screens/profile/user_profile_screen.dart
@@ -1,5 +1,6 @@
+import 'package:client/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:client/bottom_menu.dart';
+import 'package:client/widgets/bottom_nav_bar/bottom_menu.dart';
 
 
 class ProfileScreen extends StatefulWidget {
@@ -11,9 +12,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Личный кабинет'),
-      ),
+      appBar: const MyAppBar(title: "Личный кабинет"),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -29,7 +28,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ],
         ),
       ),
-      bottomNavigationBar: const BottomMenu(),
+      bottomNavigationBar: BottomMenu(selectedIndex: 3,),
     );
   }
 }
