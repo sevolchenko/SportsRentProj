3882420a27ad0b8e11fdf4e5170d51e79a8ebab8
jessi <vadim02101@gmail.com>
2023-05-05 02:48:11 +0300

    added controller for sign_in

---
+++ app/client/Renty-client/lib/controller/sign_in_controller.dart
@@ -0,0 +1,31 @@
+import 'package:client/bloc/sign_in/sign_in_bloc.dart';
+import 'package:client/widgets/flutter_toast.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInController {
+  final BuildContext context;
+  const SignInController({required this.context});
+
+  void handleSignIn(String type) {
+    try {
+      if (type == "email") {
+        //BlocProvider.of<SignInBloc>(context).state
+        final state = context.read<SignInBloc>().state;
+        String emailAddress = state.email;
+        String password = state.password;
+        if (emailAddress.isEmpty) {
+          toastInfo(msg: "Заполните поле ввода электронной почты");
+          return;
+        }
+        if (password.isEmpty) {
+          toastInfo(msg: "Заполните поле ввода пароля");
+          return;
+        }
+        try {
+          // TODO Работа с сервером
+        } catch (e) {}
+      }
+    } catch (e) {}
+  }
+}
